11939151e1687fb446b1c4c18deda87b5b086bd4
매미 <maemi@maemiui-MacBookPro.local>
2021-12-02 16:54:17 +0900

    5th

--- 5/9465.cpp
+++ 5/9465.cpp
@@ -22,10 +22,10 @@ int	main(void)
 		dp[0][1] = a[0][1];
 		dp[1][1] = a[1][1];
 
-		for (int i = 2; i <= n; i++)
+		for (int j = 2; j <= n; j++)
 		{
-			dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + a[0][i];
-			dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + a[1][i];
+			dp[0][j] = max(dp[1][j - 1], dp[1][j - 2]) + a[0][j];
+			dp[1][j] = max(dp[0][j - 1], dp[0][j - 2]) + a[1][j];
 		}
 		cout << max(dp[0][n], dp[1][n]) << "\n";
 	}
---
+++ 6/11055.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int	dp[1001];
+int	a[1001];
+
+int	main(void)
+{
+	int	n;
+	int max = 0;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+		cin >> a[i];
+	for (int i = 1; i <= n; i++)
+	{
+		int tmp = 0;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j] < a[i])
+			{
+				if (dp[j] > tmp)
+					tmp = dp[j];
+			}
+		}
+		dp[i] = tmp + a[i];
+		if (dp[i] > max)
+			max = dp[i];
+	}
+	cout << max;
+}
