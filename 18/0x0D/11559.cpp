d7af14546cde99d398f7a522db5fb2ce7d0a9cee
maemi <dbsrl759@naver.com>
2022-01-30 06:29:53 +0900

    18th

--- 18/0x0D/11559.cpp
+++ 18/0x0D/11559.cpp
@@ -1,1 +1,85 @@
 //https://www.acmicpc.net/problem/11559
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int isPuyo;
+int vis[12][6];
+string map[12];
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int ans;
+
+void resetVis()
+{
+	for (int i = 0; i < 12; i++)
+		for (int j = 0; j < 6; j++)
+			vis[i][j] = 0;
+}
+
+void delpuyo(int x, int y)
+{
+	vis[x][y] = 1;
+	char color = map[x][y];
+	queue<pair<int, int> > q;
+	vector<pair<int, int> > tmp;
+	q.push(make_pair(x, y));
+	tmp.push_back(make_pair(x, y));
+	while(!q.empty())
+	{
+		pair<int, int> now = q.front();
+		q.pop();
+		for (int d = 0; d < 4; d++)
+		{
+			int nx = now.first + dx[d];
+			int ny = now.second + dy[d];
+			if (nx < 0 || nx >= 12 || ny < 0 || ny >= 6)
+				continue;
+			if (vis[nx][ny] || map[nx][ny] == '.' || map[nx][ny] != color)
+				continue;
+			vis[nx][ny] = 1;
+			q.push(make_pair(nx, ny));
+			tmp.push_back(make_pair(nx, ny));
+		}
+	}
+	if (tmp.size() >= 4)
+	{
+		isPuyo = 1;
+		for (int i = 0; i < tmp.size(); i++)
+			map[tmp[i].first][tmp[i].second] = '.';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	for (int i = 0; i < 12; i++)
+		cin >> map[i];
+	do
+	{
+		isPuyo = 0;
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 10; j >= 0; j--)
+			{
+				int tmp = j;
+				while (tmp < 11 && map[tmp + 1][i] == '.')
+				{
+					swap(map[tmp][i], map[tmp + 1][i]);
+					tmp++;
+				}
+			}
+		}
+		for (int i = 0; i < 12; i++)
+			for (int j = 0; j < 6; j++)
+				if (!vis[i][j] && map[i][j] != '.')
+					delpuyo(i, j);
+		if(isPuyo)
+			ans++;
+		resetVis();
+	} while (isPuyo);
+	cout << ans;
+}
--- 18/0x0D/15686.cpp
+++ 18/0x0D/15686.cpp
@@ -1,14 +1,48 @@
 //https://www.acmicpc.net/problem/15686
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <utility>
 using namespace std;
 
-int n, m, r, c;
+int n, m, r, c, ans = 0x7f7f7f7f;
 int map[50][50];
+vector<pair<int, int> > chic;
+vector<pair<int, int> > house;
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> map[i][j];
+			if (map[i][j] == 1)
+				house.push_back(make_pair(i, j));
+			if (map[i][j] == 2)
+				chic.push_back(make_pair(i, j));
+		}
+	}
+	vector<int> brute(chic.size(), 1);
+	fill(brute.begin(), brute.begin() + chic.size() - m, 0);
+	do
+	{
+		int dist = 0;
+		for (int i = 0; i < house.size(); i++)
+		{
+			int tmp = 0x7f7f7f7f;
+			for (int j = 0; j < chic.size(); j++)
+			{
+				if (brute[j] == 0)
+					continue;
+				tmp = min(tmp, abs(chic[j].first - house[i].first) + abs(chic[j].second - house[i].second));
+			}
+			dist += tmp;
+		}
+		ans = min(ans, dist);
+	} while (next_permutation(brute.begin(), brute.end()));
+	cout << ans;
 }
