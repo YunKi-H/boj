f5a152f0fe1bc5534422251a542a500368a4ab4e
maemi <dbsrl759@naver.com>
2022-02-03 14:52:48 +0900

    19th

--- 19/0x0D/14500.cpp
+++ 19/0x0D/14500.cpp
@@ -1,1 +1,200 @@
 // https://www.acmicpc.net/problem/14500
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m, ans;
+int map[505][505];
+//ㅡ
+int b1(int x, int y)
+{
+	int sum = 0;
+	for (int i = 0; i < 4; i++)
+		sum += map[x][y + i];
+	return sum;
+}
+//ㅣ
+int b2(int x, int y)
+{
+	int sum = 0;
+	for (int i = 0; i < 4; i++)
+		sum += map[x + i][y];
+	return sum;
+}
+//ㅁ
+int b3(int x, int y)
+{
+	int sum = 0;
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			sum += map[x + i][y + j];
+	return sum;
+}
+//ㅜ
+int b4(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1];
+	sum += map[x + 1][y + 1] + map[x][y + 2];
+	return sum;
+}
+//ㅓ
+int b5(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y + 1] + map[x + 1][y];
+	sum += map[x + 1][y + 1] + map[x + 2][y + 1];
+	return sum;
+}
+//ㅗ
+int b6(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y + 1] + map[x + 1][y];
+	sum += map[x + 1][y + 1] + map[x + 1][y + 2];
+	return sum;
+}
+//ㅏ
+int b7(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x + 1][y];
+	sum += map[x + 1][y + 1] + map[x + 2][y];
+	return sum;
+}
+//z
+int b8(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1];
+	sum += map[x + 1][y + 1] + map[x + 1][y + 2];
+	return sum;
+}
+//z->90
+int b9(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x + 1][y];
+	sum += map[x + 1][y + 1] + map[x + 2][y + 1];
+	return sum;
+}
+//z->180
+int b10(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y + 1] + map[x][y + 2];
+	sum += map[x + 1][y] + map[x + 1][y + 1];
+	return sum;
+}
+//z->270
+int b11(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y + 1] + map[x + 1][y];
+	sum += map[x + 1][y + 1] + map[x + 2][y];
+	return sum;
+}
+
+int b12(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1];
+	sum += map[x + 1][y];
+	sum += map[x + 2][y];
+	return sum;
+}
+
+int b13(int x, int y)
+{
+	int sum = 0;
+	sum +=					map[x][y + 1];
+	sum +=					map[x + 1][y + 1];
+	sum += map[x + 2][y] + map[x + 2][y + 1];
+	return sum;
+}
+
+int b14(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y];
+	sum += map[x + 1][y];
+	sum += map[x + 2][y] + map[x + 2][y + 1];
+	return sum;
+}
+
+int b15(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1];
+	sum +=				map[x + 1][y + 1];
+	sum +=				map[x + 2][y + 1];
+	return sum;
+}
+
+int b16(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1] + map[x][y + 2];
+	sum += 							map[x + 1][y + 2];
+	return sum;
+}
+
+int b17(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y];
+	sum += map[x + 1][y] + map[x + 1][y + 1] + map[x + 1][y + 2];
+	return sum;
+}
+
+int b18(int x, int y)
+{
+	int sum = 0;
+	sum += map[x][y] + map[x][y + 1] + map[x][y + 2];
+	sum += map[x + 1][y];
+	return sum;
+}
+
+int b19(int x, int y)
+{
+	int sum = 0;
+	sum += 										map[x][y + 2];
+	sum += map[x + 1][y] + map[x + 1][y + 1] + map[x + 1][y + 2];
+	return sum;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			cin >> map[i][j];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			ans = max(ans, b1(i, j));
+			ans = max(ans, b2(i, j));
+			ans = max(ans, b3(i, j));
+			ans = max(ans, b4(i, j));
+			ans = max(ans, b5(i, j));
+			ans = max(ans, b6(i, j));
+			ans = max(ans, b7(i, j));
+			ans = max(ans, b8(i, j));
+			ans = max(ans, b9(i, j));
+			ans = max(ans, b10(i, j));
+			ans = max(ans, b11(i, j));
+			ans = max(ans, b12(i, j));
+			ans = max(ans, b13(i, j));
+			ans = max(ans, b14(i, j));
+			ans = max(ans, b15(i, j));
+			ans = max(ans, b16(i, j));
+			ans = max(ans, b17(i, j));
+			ans = max(ans, b18(i, j));
+			ans = max(ans, b19(i, j));
+		}
+	}
+	cout << ans;
+}
--- 19/0x0D/3190.cpp
+++ 19/0x0D/3190.cpp
@@ -1,16 +1,92 @@
 // https://www.acmicpc.net/problem/3190
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <deque>
 using namespace std;
 
-int n, k, ans;
-int map[100][100];
-vector<pair<int, int> > snake;
+int n, k, l, ans, x, y, d = 1;
+char c;
+int map[101][101];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+deque<pair<int, int> > snake;
+deque<pair<int, char> > turn;
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cin >> n >> k;
+	for (int i = 0; i < k; i++)
+	{
+		cin >> x >> y;
+		map[x][y] = -1;
+	}
+	cin >> l;
+	for (int i = 0; i < l; i++)
+	{
+		cin >> x >> c;
+		turn.push_back(make_pair(x, c));
+	}
+	x = 1;
+	y = 1;
+	map[x][y] = 1;
+	snake.push_back(make_pair(x, y));
+	while (1)
+	{
+		ans++;
+		x += dx[d];
+		y += dy[d];
+		if (x < 1 || x > n || y < 1 || y > n || map[x][y] == 1)
+			break;
+		if (map[x][y] == 0)
+		{
+			pair<int, int> tail = snake.front();
+			map[tail.first][tail.second] = 0;
+			snake.pop_front();
+		}
+		map[x][y] = 1;
+		snake.push_back(make_pair(x, y));
+		if (ans == turn.front().first)
+		{
+			if (turn.front().second == 'L')
+			{
+				switch (d)
+				{
+				case 0:
+					d = 3;
+					break;
+				case 1:
+					d = 0;
+					break;
+				case 2:
+					d = 1;
+					break;
+				case 3:
+					d = 2;
+					break;
+				}
+			}
+			if (turn.front().second == 'D')
+			{
+				switch (d)
+				{
+				case 0:
+					d = 1;
+					break;
+				case 1:
+					d = 2;
+					break;
+				case 2:
+					d = 3;
+					break;
+				case 3:
+					d = 0;
+					break;
+				}
+			}
+			turn.pop_front();
+		}
+	}
+	cout << ans;
 }
